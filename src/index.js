9c3b899fb67993518a12af4fd72055e6d0d51ca6
ikape <ikapeisaac3@gmail.com>
2023-04-18 06:04:26 +0100

    created the app.use middlware and request query route

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 3030;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded());
+app.use((req, res, next) => {
+  console.log(`${req.method}:${req.url}`);
+  next();
+});
+// PORT
 app.listen(PORT, () => console.log(`Running express Server on Port ${PORT}`));
 
 const groceryList = [
@@ -20,10 +26,17 @@ const groceryList = [
     quantity: 9
   }
 ];
+
+// Routes
 app.get("/store", (request, response) => {
   response.send(groceryList);
 });
 
+app.get("/store/:item", (request, response) => {
+  const { item } = request.params;
+  const groceryItem = groceryList.find((g) => g.item === item);
+  response.send(groceryItem);
+});
 app.post("/store", (request, response) => {
   console.log(request.body);
   groceryList.push(request.body);
