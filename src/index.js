9fc7d83e0a85fa9b8f003f7623011a857051cf0c
ikape <ikapeisaac3@gmail.com>
2023-04-18 05:34:49 +0100

    created the store routes for post/get request and tested with postman

--- src/index.js
+++ src/index.js
@@ -2,4 +2,30 @@ const express = require("express");
 const app = express();
 const PORT = 3030;
 
+app.use(express.json());
+app.use(express.urlencoded());
 app.listen(PORT, () => console.log(`Running express Server on Port ${PORT}`));
+
+const groceryList = [
+  {
+    item: "milk",
+    quantity: 2
+  },
+  {
+    item: "cereal",
+    quantity: 5
+  },
+  {
+    item: "cocopops",
+    quantity: 9
+  }
+];
+app.get("/store", (request, response) => {
+  response.send(groceryList);
+});
+
+app.post("/store", (request, response) => {
+  console.log(request.body);
+  groceryList.push(request.body);
+  response.send(201);
+});
